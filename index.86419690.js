03bc27723fda43cee4d3a36440620c961f1998ee
AntonPtashynskyi <86067969+AntonPtashynskyi@users.noreply.github.com>
2022-01-12 19:03:59 +0000

    Deploying to gh-pages from @ AntonPtashynskyi/project-js-demo@08fa5495a7fceebc57b27e11225f26c3c834c4c0 🚀

---
+++ index.86419690.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-modal-close]"),t=document.querySelector("[data-modal]"),c=document.querySelector("[films-list]");function d(){t.classList.toggle("is-hidden")}e.addEventListener("click",d),c.addEventListener("click",(function(e){"IMG"===e.target.nodeName&&d()}));
+//# sourceMappingURL=index.86419690.js.map
